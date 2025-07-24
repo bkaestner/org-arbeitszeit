7f8c50e1dea3595bbf3e79f5a5737f7336478a92
Benjamin Kästner <benjamin.kaestner@gmail.com>
2025-07-24 20:57:45 +0200

    Bump version to 0.0.5

--- org-arbeitszeit.el
+++ org-arbeitszeit.el
@@ -5,7 +5,7 @@
 ;; Author:  Benjamin Kästner <benjamin.kaestner@gmail.com>
 ;; URL: https://github.com/bkaestner/org-arbeitszeit
 ;; Keywords: tools, org, calendar, convenience
-;; Version: 0.0.4
+;; Version: 0.0.5
 ;; Package-Requires: ((emacs "27.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
